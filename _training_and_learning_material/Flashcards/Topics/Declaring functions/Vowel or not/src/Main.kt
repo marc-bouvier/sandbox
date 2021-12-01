6fb8f3d911d7d2756bd4db851d2fdb998d8a0eee
Marc Bouvier <m.bouvier.dev@gmail.com>
2021-12-01 10:27:48 +0100

    clear flashcards stage 7/7

--- _training_and_learning_material/Flashcards/Topics/Declaring functions/Vowel or not/src/Main.kt
+++ _training_and_learning_material/Flashcards/Topics/Declaring functions/Vowel or not/src/Main.kt
@@ -2,12 +2,6 @@
 
 fun main() {
     val letter = readLine()!!.first()
-    val isVowel: (Char) -> Boolean = { it -> it.lowercaseChar() in listOf('a', 'e', 'i', 'o', 'u') }
+
     println(isVowel(letter))
 }
-
-
-fun solution(numbers: List<Int>) =
-    numbers.filter { it % 2 == 0 }
-        .joinToString(" ")
-        .let(::println)
